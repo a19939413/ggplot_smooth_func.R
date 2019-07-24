729e254ead29e8ce9abdebcc75c2ac9568e69ba6
a19939413 <53243383+a19939413@users.noreply.github.com>
2019-07-24 09:41:44 +0800

    Update ggplot_smooth_func.R

--- ggplot_smooth_func.R
+++ ggplot_smooth_func.R
@@ -102,8 +102,8 @@ StatSmoothFunc <- ggproto("StatSmooth", Stat,
                         
                         m = model
                         eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2, 
-                                         list(a = format(coef(m)[1], digits = 3), 
-                                              b = format(coef(m)[2], digits = 3), 
+                                         list(a = format(coef(m)[[1]], digits = 3), 
+                                              b = format(coef(m)[[2]], digits = 3), 
                                               r2 = format(summary(m)$r.squared, digits = 3)))
                         func_string = as.character(as.expression(eq))
                         
